74c6f2865101d4a2d4d93234473a78d4e6cc6bf0
Om Bhongade <bhongadeom653@gmail.com>
2025-11-29 01:12:25 +0530

    Create Om3.c

---
+++ Om3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int add(int a,int b){
+   return a+b;
+}
+int main() {
+        int num1,num2,sum;
+        printf("Enter any two numbers\n");
+        scanf("%d %d ",&num1,&num2); 
+
+
+        sum=add(num1,num2);
+        printf("sum=%d\n",sum);
+        return 0;
+
+}
